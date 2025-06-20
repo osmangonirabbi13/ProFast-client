db9bf6fa11c5f3b41cdac11339868822448fb7ae
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-06-20 10:27:43 +0600

    services section add

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Banner from "../Banner/Banner";
+import Services from "../Services/Services";
 
 const Home = () => {
   return (
-    <div>
-      <h1>This is a home </h1>
-    </div>
+    <>
+      <Banner />
+      <Services />
+    </>
   );
 };
 
